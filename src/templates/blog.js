96d3178798e9744217c14ba6d4ed84155d1a92fe
Austin Robinson <aus_robinson@yahoo.com>
2019-12-20 01:43:55 -0700

    One more, adding the reference for the Container component

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import styled from 'styled-components'
 import Layout from '../components/layout'
+import Container from "../components/container"
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import Head from "../components/head"
 
