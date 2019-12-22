14260118e074b5bd8ff42417973826bdc8405324
Austin Robinson <aus_robinson@yahoo.com>
2019-12-22 11:28:29 -0700

    Site was crashing due to issues with styled-theming. Attempting to fix.

--- src/theme.js
+++ src/theme.js
@@ -1,79 +1,61 @@
-// const lightTheme = {
-//     layer0: "#f9f9f9",
-//     layer1: "#ffffff",
-//     foreground0: "#000000",
-//     foreground1: "#737373",
-//     foreground2: "#adadad",
-//     accent: "#d9ae6d",
-// }
+import theme from 'styled-theming';
 
-// const darkTheme = {
-//     layer0: "#000000",
-//     layer1: "#1a1a1a",
-//     foreground0: "ffffff",
-//     foreground1: "#737373",
-//     foreground2: "#adadad",
-//     accent: "#ead2ae",
-// }
-
-import style from 'styled-theming';
-
-export const layer0 = style('mode', {
+export const layer0 = theme('mode', {
   light: '#ebebeb',
   dark: '#000000'
 });
 
-export const layer1 = style('mode', {
+export const layer1 = theme('mode', {
   light: '#f6f6f6',
   dark: '#0f0f0f'
 });
 
-export const layer2 = style('mode', {
+export const layer2 = theme('mode', {
   light: '#ffffff',
   dark: '#1a1a1a'
 });
 
-export const foreground = style('mode', {
+export const foreground = theme('mode', {
   light: '#000000',
   dark: '#ffffff'
 });
 
-export const kindaLightForeground = style('mode', {
+export const kindaLightForeground = theme('mode', {
   light: '#404040',
   dark: '#cccccc'
 })
 
-export const lightForeground = style('mode', {
+export const lightForeground = theme('mode', {
   light: '#737373',
   dark: '#adadad'
 })
 
-export const moreTransparent = style('mode', {
+export const moreTransparent = theme('mode', {
   light: 'rgba(0,0,0,0.05)',
   dark: 'rgba(0,0,0,0.15)'
 })
 
-export const transparent = style('mode', {
+export const transparent = theme('mode', {
   light: 'rgba(0,0,0,0.1)',
   dark: 'rgba(255,255,255,0.1)'
 })
 
-export const lessTransparent = style('mode', {
+export const lessTransparent = theme('mode', {
   light: 'rgba(0,0,0,0.15)',
   dark: 'rgba(0,0,0,0.25)'
 })
 
-export const evenLessTransparent = style('mode', {
+export const evenLessTransparent = theme('mode', {
   light: 'rgba(0,0,0,0.25)',
   dark: 'rgba(0,0,0,0.40)'
 })
 
-export const evenTransparent = style('mode', {
+export const evenTransparent = theme('mode', {
   light: 'rgba(0,0,0,0.5)',
   dark: 'rgba(255,255,255,0.5)'
 })
 
-export const accent = style('mode', {
+export const accent = theme('mode', {
   light: "#d9ae6d",
   dark: "#ead2ae"
 })
